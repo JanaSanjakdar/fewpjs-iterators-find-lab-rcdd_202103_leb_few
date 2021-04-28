d712c7750c797768453aed266873ab8c6d5b59bd
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 21:47:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  {year: "2013", result: "L"},
+  {year: "2012", result: "W"},
+  {year: "2015", result: "N/A"}
